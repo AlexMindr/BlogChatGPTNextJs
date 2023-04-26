525f6e9befe0e7be1e5251fc433163716b86854b
AlexMindr <49548048+AlexMindr@users.noreply.github.com>
2023-04-26 16:19:13 +0300

    first commit with init

---
+++ app/post/loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Loading() {
+  return (
+    <div className="border-[16px] rounded-[50%] border-solid border-accent-red w-32 h-32 animate-spin"></div>
+  );
+}
+
+export default Loading;
