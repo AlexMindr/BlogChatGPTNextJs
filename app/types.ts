c6a324ab95b0f5b9e141891dbd59c533c16060f6
AlexMindr <49548048+AlexMindr@users.noreply.github.com>
2023-05-04 20:41:02 +0300

    post dynamic id page w/o tiptap editor

---
+++ app/types.ts
@@ -0,0 +1,11 @@
+export interface FormattedPostType {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  title: string;
+  category: string;
+  content: string;
+  author: string;
+  image: string;
+  snippet: string;
+}
